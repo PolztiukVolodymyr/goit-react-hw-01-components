be3b0f1cbd4f0a0f1ebd933c8ffcc8621dea96d1
PolztiukVolodymyr <92870556+PolztiukVolodymyr@users.noreply.github.com>
2022-07-03 00:25:40 +0300

    task123

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import css from './FriendList.module.css';
+import FriendListItem from "./FriendListItem"
+
+export default function FriendList({ friends }) {
+    return (
+        <ul className={css.friendList}>
+            {friends.map(({ id, avatar, name, isOnline }) => (
+                <FriendListItem
+                    key={id}
+                    isOnline={isOnline}
+                    name={name}
+                    avatar={avatar} />
+            ))}
+        </ul>);
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+            name: PropTypes.string.isRequired,
+            avatar: PropTypes.string.isRequired,
+   })
+    )
+}
